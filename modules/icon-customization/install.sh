6b1b05acfefdeb88a88e9b7ecaf557fa1ce5b7e2
adryd <me@adryd.com>
2026-06-11 19:14:41 -0400

    icon-customization: update paths

--- modules/icon-customization/install.sh
+++ modules/icon-customization/install.sh
@@ -7,15 +7,16 @@ AR_MODULE="icon-customization"
 if [[ -e "/var/lib/flatpak/app/md.obsidian.Obsidian/current/active/" ]]; then
     sudo cp -f ./icons/obsidian-512.png /var/lib/flatpak/app/md.obsidian.Obsidian/current/active/files/share/icons/hicolor/512x512/apps/md.obsidian.Obsidian.png
 fi
+
 if [[ -e "/var/lib/flatpak/app/org.signal.Signal/current/active/" ]]; then
     sudo cp -f ./icons/signal-512.png /var/lib/flatpak/app/org.signal.Signal/current/active/files/share/icons/hicolor/512x512/apps/org.signal.Signal.png
     sudo cp -f ./icons/signal-256.png /var/lib/flatpak/app/org.signal.Signal/current/active/files/share/icons/hicolor/256x256/apps/org.signal.Signal.png
     sudo cp -f ./icons/signal-128.png /var/lib/flatpak/app/org.signal.Signal/current/active/files/share/icons/hicolor/128x128/apps/org.signal.Signal.png
 fi
-if [[ -e "/usr/share/applications/signal-desktop.desktop" ]]; then
-    sudo cp -f ./icons/signal-512.png /usr/share/icons/hicolor/512x512/apps/signal-desktop.png
-    sudo cp -f ./icons/signal-256.png /usr/share/icons/hicolor/256x256/apps/signal-desktop.png
-    sudo cp -f ./icons/signal-128.png /usr/share/icons/hicolor/128x128/apps/signal-desktop.png
+if [[ -e "/usr/share/applications/signal.desktop" ]]; then
+    sudo cp -f ./icons/signal-512.png /usr/share/icons/hicolor/512x512/apps/signal.png
+    sudo cp -f ./icons/signal-256.png /usr/share/icons/hicolor/256x256/apps/signal.png
+    sudo cp -f ./icons/signal-128.png /usr/share/icons/hicolor/128x128/apps/signal.png
 fi
 
 # firefox icon cut out from https://www.instagram.com/firefox/p/DCHmrMlS0Ny/
