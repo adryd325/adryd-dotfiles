ecbaecd4108c9e140247bb8a416fc34700edf7de
adryd <me@adryd.com>
2026-08-26 12:05:08 -0400

    icon-customization: librewolf is a work profile

--- modules/icon-customization/install.sh
+++ modules/icon-customization/install.sh
@@ -13,6 +13,7 @@ if [[ -e "/var/lib/flatpak/app/org.signal.Signal/current/active/" ]]; then
     sudo cp -f ./icons/signal-256.png /var/lib/flatpak/app/org.signal.Signal/current/active/files/share/icons/hicolor/256x256/apps/org.signal.Signal.png
     sudo cp -f ./icons/signal-128.png /var/lib/flatpak/app/org.signal.Signal/current/active/files/share/icons/hicolor/128x128/apps/org.signal.Signal.png
 fi
+
 if [[ -e "/usr/share/applications/signal.desktop" ]]; then
     sudo cp -f ./icons/signal-512.png /usr/share/icons/hicolor/512x512/apps/signal.png
     sudo cp -f ./icons/signal-256.png /usr/share/icons/hicolor/256x256/apps/signal.png
@@ -24,3 +25,11 @@ if [[ -e "/usr/share/applications/org.mozilla.firefox.desktop" ]]; then
     sudo cp -f ./icons/firefox-256.png /usr/share/icons/hicolor/256x256/firefox.png
     sudo cp -f ./icons/firefox-256.png /usr/share/icons/hicolor/256x256/apps/firefox.png
 fi
+
+if [[ -e "/var/lib/flatpak/app/io.gitlab.librewolf-community/current/active/" ]]; then
+    sudo cp -f ./icons/librewolf-256.png /var/lib/flatpak/app/io.gitlab.librewolf-community/current/active/files/share/icons/hicolor/128x128/apps/io.gitlab.librewolf-community.png
+fi
+
+
+sudo gtk-update-icon-cache
+xdg-desktop-menu forceupdate
